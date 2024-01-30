a408bd9c61c6a42add60e056aa02686457e750c8
logikfabrik <logikfabrik@users.noreply.github.com>
2024-01-30 09:56:50 +0100

    Add sample console

---
+++ src/SoporNu.Extensions.Microsoft.DependencyInjection.Samples.Console/AvsService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SoporNu.Extensions.Microsoft.DependencyInjection.Samples.Console
+{
+    internal sealed class AvsService(ISoporApiClient client) : IHostedService
+    {
+        private readonly ISoporApiClient _client = client;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            foreach (var avs in await _client.GetAllAvs(cancellationToken))
+            {
+                System.Console.WriteLine($"{avs.Municipality.Name}: {avs.PrimaryName ?? avs.SecondaryName}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/SoporNu.Extensions.Microsoft.DependencyInjection.Samples.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using SoporNu.Extensions.Microsoft.DependencyInjection;
+using SoporNu.Extensions.Microsoft.DependencyInjection.Samples.Console;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSoporApiClient();
+        services.AddHostedService<AvsService>();
+    })
+    .Build();
+
+await host.RunAsync();
